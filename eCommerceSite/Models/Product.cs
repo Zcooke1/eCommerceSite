3f562da7625353f572357e22c663ed38fc466360
Zcooke1 <115667096+Zcooke1@users.noreply.github.com>
2023-03-10 13:38:26 -0800

    Product class didnt update, made it again.

--- eCommerceSite/Models/Product.cs
+++ eCommerceSite/Models/Product.cs
@@ -1,6 +1,15 @@
-﻿namespace eCommerceSite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceSite.Models
 {
     public class Product
     {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        [Range(0, 1000)]
+        public double Price { get; set; }
     }
-}
