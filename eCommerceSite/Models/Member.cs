2cddf96e8d3cf5aa58424258a0f336a2100e73f1
Zcooke1 <115667096+Zcooke1@users.noreply.github.com>
2023-03-11 14:53:25 -0800

    Added register view model

--- eCommerceSite/Models/Member.cs
+++ eCommerceSite/Models/Member.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Composition.Convention;
 
 namespace eCommerceSite.Models
 {
@@ -15,5 +16,27 @@ namespace eCommerceSite.Models
 
         public string Username { get; set; }
 
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name ="Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof (Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
     }
 }
