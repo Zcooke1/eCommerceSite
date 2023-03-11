cff53a6da78fe07470929feae5d1568626638737
Zcooke1 <115667096+Zcooke1@users.noreply.github.com>
2023-03-11 14:41:47 -0800

    Added migration for member class

--- eCommerceSite/Models/Member.cs
+++ eCommerceSite/Models/Member.cs
@@ -1,6 +1,19 @@
-﻿namespace eCommerceSite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceSite.Models
 {
     public class Member
     {
+        [Key]
+        public int MemberId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Username { get; set; }
+
     }
 }
