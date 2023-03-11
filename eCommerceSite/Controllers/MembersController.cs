b7640fd113c6d63222434de5ea9b8524a172e29e
Zcooke1 <115667096+Zcooke1@users.noreply.github.com>
2023-03-11 15:20:53 -0800

    Made Member Phone and Username optionally Added HTTPPost for register

--- eCommerceSite/Controllers/MembersController.cs
+++ eCommerceSite/Controllers/MembersController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerceSite.Controllers
 {
+    
     public class MembersController : Controller
     {
+        private readonly ProductContext _context;
+        public MembersController(ProductContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel regModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Member newMember = new()
+                {
+                    Email = regModel.Email,
+                    Password = regModel.Password
+                };
+                _context.Members.Add(newMember);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(regModel);
         }
     }
 }
