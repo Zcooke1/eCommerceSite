7cd07f6cec957dadf73fd44249d9e9bd245364cd
Zcooke1 <115667096+Zcooke1@users.noreply.github.com>
2023-03-11 13:38:21 -0800

    changed 1 character in edit function

--- eCommerceSite/Controllers/ProductsController.cs
+++ eCommerceSite/Controllers/ProductsController.cs
@@ -48,12 +48,27 @@ namespace eCommerceSite.Controllers
         {
             Product? productToEdit = await _context.Products.FindAsync(id);
 
-            if(productToEdit != null)
+            if (productToEdit == null)
             {
                 return NotFound();
             }
 
             return View(productToEdit);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Update(productModel);
+                await _context.SaveChangesAsync();
+
+                TempData["Message"] = $"{productModel.ProductName} was updated successfully!";
+                return RedirectToAction("Index");
+            }
+
+            return View(productModel);
         }
 
     }
