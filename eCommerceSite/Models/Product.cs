57e50da400fa719991aa75f57ee69ea3d4d41916
Zcooke1 <115667096+Zcooke1@users.noreply.github.com>
2023-03-12 15:33:04 -0700

    Added ViewModel for the shopping cart summary page

--- eCommerceSite/Models/Product.cs
+++ eCommerceSite/Models/Product.cs
@@ -13,4 +13,17 @@ namespace eCommerceSite.Models
         [Range(0, 1000)]
         public double Price { get; set; }
     }
+
+    /// <summary>
+    /// A single product that has been added to the users
+    /// shopping cart cookie
+    /// </summary>
+    public class CartProductViewModel
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+    }
 }
