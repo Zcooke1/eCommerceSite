486a3385e3fd076af723604d6858eaadff499b9b
Zcooke1 <115667096+Zcooke1@users.noreply.github.com>
2023-03-12 15:33:39 -0700

    Coded Add to cart, remove from cart, and added a cart summary page.

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -1,12 +1,14 @@
 ﻿using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace eCommerceSite.Controllers
 {
     public class CartController : Controller
     {
         private readonly ProductContext _context;
+        private const string Cart = "ShoppingCart";
 
         public CartController(ProductContext context)
         {
@@ -23,10 +25,67 @@ namespace eCommerceSite.Controllers
                 TempData["Message"] = "Sorry that product no longer exists";
                 return RedirectToAction("Index", "Products");
             }
-            
-            
+
+            CartProductViewModel cartProduct = new()
+            {
+                ProductId = productToAdd.ProductId,
+                ProductName = productToAdd.ProductName,
+                Price = productToAdd.Price
+            };
+
+            List<CartProductViewModel> cartProducts = GetExistingCartData();
+            cartProducts.Add(cartProduct);
+            WriteShoppingCartCookie(cartProducts);
+
             TempData["Message"] = "Item added to cart";
             return RedirectToAction("Index", "Products");
+        }
+
+        private void WriteShoppingCartCookie(List<CartProductViewModel> cartProducts)
+        {
+            string cookieData = JsonConvert.SerializeObject(cartProducts);
+
+            HttpContext.Response.Cookies.Append(Cart, cookieData, new CookieOptions()
+            {
+                Expires = DateTimeOffset.Now.AddYears(1)
+            });
+        }
+
+        /// <summary>
+        /// Return the current list of products in the users shopping
+        /// cart cookie. If there is no cookie, an empty list will be returned
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private List<CartProductViewModel> GetExistingCartData()
+        {
+            string? cookie = HttpContext.Request.Cookies[Cart];
+            if (string.IsNullOrWhiteSpace(cookie))
+            {
+                return new List<CartProductViewModel>();
+            }
+            return JsonConvert.DeserializeObject<List<CartProductViewModel>>(cookie);
+        }
+
+        public IActionResult Summary()
+        {
+            //Read shopping cart data and convert to list of view model
+            List<CartProductViewModel> cartProducts = GetExistingCartData();
+            return View(cartProducts);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<CartProductViewModel> cartProducts = GetExistingCartData();
+
+            CartProductViewModel? targetProduct =
+                cartProducts.Where(p => p.ProductId == id).FirstOrDefault();
+
+            cartProducts.Remove(targetProduct);
+
+            WriteShoppingCartCookie(cartProducts);
+
+            return RedirectToAction(nameof(Summary));
         }
     }
 }
